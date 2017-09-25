3d1526d52b8395b198918619a5c8293e11fc1894
Emanuel Calderoni <emanuelcalderoni@MacBook-Pro-de-Emanuel.local>
2017-09-25 18:46:48 -0300

    cambios remotos

--- GithubProject4/ViewController.swift
+++ GithubProject4/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         // comment agregado
         //local changes
+        // remote changes
     }
 
     override func didReceiveMemoryWarning() {
